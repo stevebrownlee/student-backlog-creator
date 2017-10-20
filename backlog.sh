82d5d0eef50156d686852a7d47b7c4e7020fed2b
Steve Brownlee <chortlehoort@gmail.com>
2017-10-20 14:51:10 -0500

    Now accepts a second argument for the source repo

--- backlog.sh
+++ backlog.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-printf "Issues > "
-read ISSUES
 
-IN=$(<$1)
-set -- "$IN" 
-IFS=";"; declare -a foo=($*) 
+  printf "Issues > "
+  read ISSUES
+  SOURCE=${2}
 
-for student in "${foo[@]}"
-  do
-    echo "python gh-issues-import.py --source stevebrownlee/personal-site --target ${student}/${student}.github.io --issues ${ISSUES}"
-    python gh-issues-import.py --source stevebrownlee/personal-site --target ${student}/${student}.github.io --issues ${ISSUES}
-  done
+  IN=$(<$1)
+  set -- "$IN"
+  IFS=";"; declare -a foo=($*) 
 
-
+  for student in "${foo[@]}"
+    do
+      echo "python gh-issues-import.py --source ${SOURCE} --target ${student}/${student}.github.io --issues ${ISSUES}"
+      python gh-issues-import.py --source ${SOURCE} --target ${student}/${student}.github.io --issues ${ISSUES}
+    done
